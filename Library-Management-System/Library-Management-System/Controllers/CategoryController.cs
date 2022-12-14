80613048838c86182ff083a3c2544b0b4b60d3c6
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-14 18:16:46 +0200

    Linq sorguları için kütüphane eklendi ve veri tabanı ismi ile bağlantı sağlanılarak kategori tablosu listelendi.

---
+++ Library-Management-System/Library-Management-System/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+namespace Library_Management_System.Controllers
+{
+    public class CategoryController : Controller
+    {
+        devrimme_senaEntities db=new devrimme_senaEntities();
+        // GET: Category
+        public ActionResult Index()
+        {
+
+            var degerler = db.Category.ToList();
+            return View(degerler);
+        }
+
+    }
+}
