3f4edb4550ab9229e95eb5ebc748171ef8080c10
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-15 23:13:24 +0200

    category tablosunda kategori ekle ve sil işlemleri yapıldı

--- Library-Management-System/Library-Management-System/Controllers/CategoryController.cs
+++ Library-Management-System/Library-Management-System/Controllers/CategoryController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Library_Management_System.Models.Entity;
+using Library_Management_System.Models.Entity; //oluşturduğun projeyi model ile birlikte yani sql de oluşturduğun tabloları proje ile birleştirmek için yazılır. ve her controller de yaz.
+                                               //MODELİN İÇERİSİNDE BULUNAN DEĞERLERE ULAŞMAKTIR
 namespace Library_Management_System.Controllers
 {
     public class CategoryController : Controller
     {
-        devrimme_senaEntities db=new devrimme_senaEntities();
+        devrimme_senaEntities db=new devrimme_senaEntities();//bağlantı sağlanması için yazılır.
         // GET: Category
         public ActionResult Index()
         {
@@ -16,6 +17,29 @@ namespace Library_Management_System.Controllers
             var degerler = db.Category.ToList();
             return View(degerler);
         }
+        [HttpGet]//refresh 
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]//
+        public ActionResult AddCategory(Category p ) //parametre tanımlanmasının sebebi kategori tablosundan alsın p ye atsın.
+        {
+            db.Category.Add(p);// kategori tablosuna eklesin kategoriye eklenen değerleri
+            db.SaveChanges();
+            return View();//ne yazdıysan sadece onu gösterir.
+        }
+        public ActionResult DeleteCategory(int id) //parametre id olacak çünkü id ye göre silme işlemi gerçekleştirecek.
+        {
+            var category = db.Category.Find(id);//find bul demek.
+            db.Category.Remove(category); 
+            db.SaveChanges();
+            return RedirectToAction("AddCategory");//BAŞKA SAYFAYA YÖNLENDİRİR.
+        }
+        //public ActionResult BringCategory(int id) //id ye göre parametre getirecek.
+        //{
+        //    return
+        //}
 
     }
 }
