cd1ce29ed3017f0bd4438d9c58c28c6140639060
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-16 13:27:26 +0200

    yazar ekleme işleminde httpget ve httppost işlemleri yapıldı

---
+++ Library-Management-System/Library-Management-System/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+namespace Library_Management_System.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        devrimme_senaEntities db = new devrimme_senaEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.Author.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult AddAuthor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAuthor(Category p) 
+        { 
+            db.Category.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
