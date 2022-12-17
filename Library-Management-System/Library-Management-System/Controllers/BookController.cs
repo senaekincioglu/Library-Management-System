762a783e347210823937190d338cf461bdb904b9
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-17 17:33:19 +0200

    kitap tablosuna ait index ekleme işlemi yapıldı

---
+++ Library-Management-System/Library-Management-System/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+
+namespace Library_Management_System.Controllers
+{
+    public class BookController : Controller
+    {
+        devrimme_senaEntities db = new devrimme_senaEntities();
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = db.Book.ToList();
+
+            return View(books);
+        }
+    }
+}
