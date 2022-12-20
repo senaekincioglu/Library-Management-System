a8b5b30138a7286c627905c6fb70d07dfd58439f
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-20 09:25:39 +0200

    employee controller oluşturuldu

--- Library-Management-System/Library-Management-System/Controllers/EmployeeController.cs
+++ Library-Management-System/Library-Management-System/Controllers/EmployeeController.cs
@@ -16,5 +16,40 @@ namespace Library_Management_System.Controllers
             return View(degerler);
             
         }
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee p) 
+        {
+            if(!ModelState.IsValid) //eğer boşsa bunu yapıyor.
+            {
+                return View("AddEmployee");
+            }
+            db.Employee.Add(p);//sağlandıysa bu işlemler gerçekleştirsin.
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteEmployee(int id) 
+        {
+            var person = db.Employee.Find(id);
+            db.Employee.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BringEmployee(int id) 
+        {
+            var prs = db.Employee.Find(id);
+            return View("BringEmployee", prs);
+        }
+        public ActionResult UpdateEmployee(Employee p)
+        {
+            var prs = db.Employee.Find(p.Id);
+            prs.Employees = p.Employees;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Library-Management-System/Library-Management-System/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+
+namespace Library_Management_System.Controllers
+{
+    public class MemberController : Controller
+    {
+        devrimme_senaEntities db = new devrimme_senaEntities();
+        // GET: Member
+        public ActionResult Index()
+        {
+            var degerler = db.Member.ToList();
+            return View(degerler);
+        }
+    }
+}
