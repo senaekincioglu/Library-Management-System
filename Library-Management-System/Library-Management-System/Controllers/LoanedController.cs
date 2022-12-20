5fb6903f4433b78b38cea9b4446956d226a7c78b
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-20 22:21:32 +0200

    ödünç kitap verme için back-end controller işlemleri yapıldı

---
+++ Library-Management-System/Library-Management-System/Controllers/LoanedController.cs
@@ -0,0 +1,34 @@
+﻿using Library_Management_System.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library_Management_System.Models.Entity;
+using System.Web.Mvc;
+
+
+namespace Library_Management_System.Controllers
+{
+    public class LoanedController : Controller
+    {
+        // GET: Loaned
+        devrimme_senaEntities db=new devrimme_senaEntities();   
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult LendMe()//kütüphaneden ödünç verilecek
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult LendMe(Movement p) 
+        {
+            db.Movement.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
