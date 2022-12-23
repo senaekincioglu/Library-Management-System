1ead2eef327f1cda6d29d8b3c42a99c7a8de5599
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-23 11:23:06 +0200

    işlemler tablosuna index e ait back-end işlemleri yapıldı

---
+++ Library-Management-System/Library-Management-System/Controllers/ProcessController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+
+namespace Library_Management_System.Controllers
+{
+    public class ProcessController : Controller
+    {
+        // GET: Process
+        devrimme_senaEntities1 db=new devrimme_senaEntities1();
+        public ActionResult Index()
+        {
+            var deger = db.Movement.Where(x => x.TransactionStatus == true).ToList();
+            return View(deger); //durumu true olanları listele ve göster.
+        }
+    }
+}
