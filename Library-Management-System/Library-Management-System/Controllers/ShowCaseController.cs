67f07dacdbbdfe06ebc839c78bd78dcefe9a3ab6
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-25 14:24:49 +0200

    Tüm kitap fotoğrafları dinamik olarak listelendi.

--- Library-Management-System/Library-Management-System/Controllers/ShowCaseController.cs
+++ Library-Management-System/Library-Management-System/Controllers/ShowCaseController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
 
 namespace Library_Management_System.Controllers
 {
     public class ShowCaseController : Controller
     {
         // GET: ShowCase
+        devrimme_senaEntities1 db=new devrimme_senaEntities1();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.Book.ToList();
+            return View(degerler);
         }
     }
 }
