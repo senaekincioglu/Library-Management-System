dffb589334fc12a5fa9a0c83a2d0fbbec5491ff5
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-28 12:13:19 +0200

    slider galeri ile birlikte resim yükleme işlemleri gerçekleştirildi

--- Library-Management-System/Library-Management-System/Controllers/StatisticsController.cs
+++ Library-Management-System/Library-Management-System/Controllers/StatisticsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,6 +25,16 @@ namespace Library_Management_System.Controllers
         public ActionResult Gallery()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult UploadPicture(HttpPostedFileBase dosya) //dosya yükleme kontrolüdür.
+        {
+            if(dosya.ContentLength>0)
+            {
+                string dosyayolu = Path.Combine(Server.MapPath("~/web2/resimler/"), Path.GetFileName(dosya.FileName));
+                dosya.SaveAs(dosyayolu);
+            }
+            return RedirectToAction("Gallery");
         }
 
     }
