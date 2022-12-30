d46d29d49f5506be6a053324b481566bc03f68f0
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-12-30 21:23:28 +0200

    öğrenci paneli ve login işlemleri gerçekleştirildi

---
+++ Library-Management-System/Library-Management-System/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library_Management_System.Models.Entity;
+
+namespace Library_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        devrimme_senaEntities db = new devrimme_senaEntities();
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(Member p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("LogIn");
+            }
+            db.Member.Add(p);
+            db.SaveChanges();
+            return View();
+            
+        }
+    }
+}
---
+++ Library-Management-System/Library-Management-System/Controllers/MyPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class MyPanelController : Controller
+    {
+        // GET: MyPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
